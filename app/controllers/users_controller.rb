e753c7d300b6f75a68852d6db5216c9e1e7b9e20
Jason Berkower <jasonberkower@Jasons-iMac.home>
2021-05-11 19:12:10 -0400

    Readded user controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,27 @@
 class UsersController < ApplicationController
+  def index
 
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def user_params
+    
+  end
 end
