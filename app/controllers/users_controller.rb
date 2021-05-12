5132689cd069835150fbe5f8145ebca20442fc02
Jason Berkower <jasonberkower@Jasons-iMac.home>
2021-05-12 10:32:08 -0400

    Re-completed User controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,36 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :create, :update, :destroy]
   def index
     @users: User.all
     render json: @users
   end
 
   def show
-
+    render json: @user, include: :characters, status: :ok
   end
 
   def create
-
+    @user = User.create!(user_params)
+    render json: @user
   end
 
   def update
-
+    @user = User.update(user_params)
+    render json: @dojo
   end
 
   def destroy
-
+    @user.destroy
+    render json: message{"#{@user.username} has been deleted!"}
   end
 
   private
 
   def user_params
-    
+    params.require(:user).permit(:username, :email, :password_digest)
+  end
+
+  def set_user
+    @users = User.find(params[:id])
   end
 end
