55a88251222002fdba8235a21cc606d4b14e3c3f
Jason Berkower <jasonberkower@Jasons-iMac.home>
2021-05-12 19:11:38 -0400

    Arranged potential routes.

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,10 @@ Rails.application.routes.draw do
 end
 
 # Route for Users
-
+get "/users", to: "users#all_users"
 # Route for Characters
-
+get "/characters", to: "characters#all_characters"
 # Route for Posts
-
+get "/posts", to: "posts#all_posts"
 # Route for Comments
+get "/comments", to: "comments#all_comments"
