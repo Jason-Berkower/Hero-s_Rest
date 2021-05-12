401a74df0c97c766653d6284f69bc57fd367584c
Jason Berkower <jasonberkower@Jasons-iMac.home>
2021-05-12 11:23:57 -0400

    Re-added basic copy to seed.rb.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Character.destroy_all
+Post.destroy_all
+Comment.destroy_all
+
+# USER LIST
+
+puts "#{User.count} users created!"
+# CHARACTER LIST
+
+puts "#{Character.count} characters created!"
+# POST LIST
+
+puts "#{Post.count} posts created!"
+# COMMENT LIST
+
+puts "#{Comment.count} comments created!"
