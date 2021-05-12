8fc750cf38b148ec2cc48bcf4ddbcd9b876632a2
Jason Berkower <jasonberkower@Jasons-iMac.home>
2021-05-12 11:45:44 -0400

    Added post seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,13 +16,17 @@ Comment.destroy_all
 @serious36 = User.create!(username: 'Serious36', email: 'seriouslyguys@36ers.co.uk', password_diget: 's56g7[p_iaGhD2?sO=')
 @bigbys_fan29 = User.create!(username: 'Bigbys_fan29', email: 'floatinghands@bigbyhome.deu', password_digest: 'BiGbYsMiGhTyHaNd2')
 puts "#{User.count} users created!"
+
 # CHARACTER LIST
 @periwinkle_petorax = Character.create!(name: 'Periwinkle Petorax', race: 'Fae-Touched Tiefling', class: 'Druid', img_url: 'https://cdnb.artstation.com/p/assets/images/images/024/262/295/large/taras-susak-widow.jpg?1581859803', user: @serious36)
 @bigby = Character.create!(name: 'Bigby', race: 'Human', class: 'Wizard', img_url: 'https://i.pinimg.com/originals/b7/0a/2e/b70a2e2d26948893534bf88f49d24d98.jpg', user: @bigbys_fan29)
 @proff_grizz = Character.create!(name: 'Professor Grizz Gristlethatch', race: 'Bearfolk', class: 'Artificer', img_url: 'https://cdnb.artstation.com/p/assets/images/images/037/608/151/large/rudy-siswanto-profesor-zoomancy-color2.jpg?1620829612', user: @table_master)
 puts "#{Character.count} characters created!"
-# POST LIST
 
+# POST LIST
+@requesting_extra_rations = Post.create!(title: 'Requesting Extra Rations', content: "I can't believe it, but I left town without packing enough food. Does anyone have some extra rations they can spare?", character: @periwinkle_petorax)
+@expanding_spellbooks = Post.create!(title: 'Expanding Spellbooks', content: "Finally, I've reached the end of my fifth spellbook. Not to worry; I'll simply shove more pages into it! Take that, Mordenkainen!", character: @bigby)
+@a_note_about_butterflies = Post.create!(title: 'A Note About Butterflies', content: "Butterflies are pretty.", character: @proff_grizz)
 puts "#{Post.count} posts created!"
 # COMMENT LIST
 
