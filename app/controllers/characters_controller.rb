7162e6441eb2a9c931b728e29099253d6471d482
Jason Berkower <jasonberkower@Jasons-iMac.home>
2021-05-12 10:37:19 -0400

    Recompleted Character Controller.

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,28 +1,36 @@
 class CharactersController < ApplicationController
   def index
-    @characters: Character.all
-    render json: @characters
+    @user = User.find(params[:user_id])
+    @characters: Character.where(user_id: @user.id)
+    render json: @characters, include: :user, status: :ok
   end
 
   def show
-
+    render json: @character, include: :user, status: :ok
   end
 
   def create
-
+    @characters = Character.create!(character_params)
+    render json: @character
   end
 
   def update
-
+    @character.update(character_params)
+    render json: @character
   end
 
   def destroy
-
+    @character.destroy
+    render json: message{"#{@character.name} has been deleted!"}
   end
 
   private
 
   def character_params
-    
+    params.require(:character).permit(:name, :race, :class, :img_url, :user_id)
+  end
+
+  def set_character
+    @character = Character.find(params[:id])
   end
 end
