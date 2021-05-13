8d8ddc38410d5c304724cae0ddcb8cbf0eb67f29
Jason Berkower <jasonberkower@Jasons-iMac.home>
2021-05-13 17:34:41 -0400

    Added mechanics for create comment.

--- client/src/components/Forms/Comments/CreateComment.jsx
+++ client/src/components/Forms/Comments/CreateComment.jsx
@@ -1,6 +1,24 @@
+import { useState } from 'react';
+import { createComment } from '../../../services/apiConfig';
 import './Comment.css';
 
 export default function CreateComment() {
+  const [input, setInput] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await createComment(input);
+    return res;
+  };
+
   return (
     <div>
 
