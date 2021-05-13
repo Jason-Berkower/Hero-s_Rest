1db6dcd92cd49d19dab5fdfe7eb95aa8b6fb40bc
Jason Berkower <jasonberkower@Jasons-iMac.home>
2021-05-13 16:57:49 -0400

    Created form for Post creation.

--- client/src/components/Forms/Posts/CreatePost.jsx
+++ client/src/components/Forms/Posts/CreatePost.jsx
@@ -21,7 +21,14 @@ export default function CreatePost() {
 
   return (
     <div>
-
+      <h2>Create a Post!</h2>
+      <form id="post-container" onChange={handleChage} onSubmit={handleSubmit}>
+        <label>Title:</label>
+        <input name="title" type="text" value={input.title} />
+        <label>Content:</label>
+        <input name="content" type="textarea" value={input.content} />
+        <button type="submit" />
+      </form>
     </div>
   )
 }
