3d94638cca92effe045c56ff25a8ce8f85b08ebd
Jason Berkower <jasonberkower@Jasons-iMac.home>
2021-05-16 16:01:28 -0400

    Adding logic to signin form.

--- client/src/components/SignUp/SignIn.jsx
+++ client/src/components/SignUp/SignIn.jsx
@@ -1,6 +1,30 @@
+import { getOneUser } from '../../services/apiConfig';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import './Signing.css';
 
 export default function SignIn(props) {
+  const defaultInput = {
+    username: '',
+    password_digest: '',
+  };
+
+  let history = useHistory();
+
+  const [input, setInput] = useState(defaultInput);
+
+  const handleChange = (e) => {
+    let { name, value } = e.target;
+    setInput((prevInput) => ({
+      ...prevInput,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+
+  };
+
   return (
     <div>
 
